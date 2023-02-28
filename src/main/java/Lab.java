8cd1f2b3d630f98864103fc82c0902f634f3418b
Kodie Kuiper <antonio.pennyman@hyperisland.se>
2023-02-28 00:17:23 +0000

    loginLab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,13 @@ public class Lab {
      * @return true if there is a successful login, and false otherwise.
      */
     public boolean login(String username, String password){
-        return false;
+        if (username.equals("admin") && password.equals("qwerty")) {
+            return true;
+        } else if (username.equals("user") && password.equals("password")) {
+            return true;
+        } else {
+            return false;
+        }
     }
+    
 }
